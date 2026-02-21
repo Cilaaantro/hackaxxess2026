65db70a1836e3877c74d6e80ad99ebca920892fa
root <hieu.q.tran532@gmail.com>
2026-02-21 16:31:45 -0600

    tss integrated with stt

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,33 @@
 // frontend/src/App.jsx
 import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./Login";
+import UploadAudio from "./pages/UploadAudio";
 
 function App() {
   const [user, setUser] = useState(null);
 
   return (
-    <div style={{ padding: 40 }}>
-      {!user ? (
-        <Login setUser={setUser} />
-      ) : (
-        <>
-          <p>Welcome {user.displayName}</p>
-          <p>Email: {user.email}</p>
-        </>
-      )}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div style={{ padding: 40 }}>
+              {!user ? (
+                <Login setUser={setUser} />
+              ) : (
+                <>
+                  <p>Welcome {user.displayName}</p>
+                  <p>Email: {user.email}</p>
+                </>
+              )}
+            </div>
+          }
+        />
+        <Route path="/upload" element={<UploadAudio />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
